094bd6cd71a49d7f3b9220264a1750247201b84b
Danijel Šugar <danijel.sugar@outlook.com>
2019-02-17 14:46:00 +0100

    Finished reservation entity

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -19,4 +19,128 @@ class Reservation
      */
     private $id;
 
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Room", inversedBy="id")
+     * @ORM\JoinColumn(referencedColumnName="id")
+     */
+    private $room;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $datefrom;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateto;
+
+    /**
+     * @ORM\Column(type="string", length=180)
+     * @Assert\NotBlank()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default":"0"})
+     */
+    private $status;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoom()
+    {
+        return $this->room;
+    }
+
+    /**
+     * @param mixed $room
+     */
+    public function setRoom($room): void
+    {
+        $this->room = $room;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatefrom()
+    {
+        return $this->datefrom;
+    }
+
+    /**
+     * @param mixed $datefrom
+     */
+    public function setDatefrom($datefrom): void
+    {
+        $this->datefrom = $datefrom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateto()
+    {
+        return $this->dateto;
+    }
+
+    /**
+     * @param mixed $dateto
+     */
+    public function setDateto($dateto): void
+    {
+        $this->dateto = $dateto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status): void
+    {
+        $this->status = $status;
+    }
+
+
 }
